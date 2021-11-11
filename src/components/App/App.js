2f0fa1dbbf55ce2840744585c471536a1a229f12
Carlos Gomez <carlosalbertogomez108@gmail.com>
2021-11-10 21:42:00 -0700

    Create and style landing page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,17 @@
+import * as React from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom'
+import LandingPage from '../LandingPage/LandingPage'
 
 function App() {
 
   return (
     <div className="App">
-      
+      <Switch>
+        <Route exact path='/'>
+          <LandingPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import Buttons from './Buttons';
+import './LandingPage.css'
+
+const LandingPage = (props) => {
+  return ( 
+    <div className='landing-page'>
+      <div className='welcome-container'>
+        <h1>Welcome to city finder</h1>
+        <h2>We can help you decide where to move next</h2>
+        <h3>Choose the size of the city you want to live next</h3>
+      </div>
+      <Buttons/>
+    </div>
+   );
+}
+ 
+export default LandingPage;
