3ad47a12199a3dababaf3eace4e7bbf0cd556b15
Carlos Gomez <carlosalbertogomez108@gmail.com>
2021-11-11 22:30:47 -0700

    Get all data structure to render vity overview

--- src/components/Overview/Overview.js
+++ src/components/Overview/Overview.js
@@ -13,19 +13,30 @@ const Overview = ( { match } ) => {
   useEffect(() => {
     getCitySummary(city)
     .then(data => {
+      // console.log(data)
       const cityObject = {
-        coordinates: data.coordinates,
+        // coordinates: data.coordinates,
+        latitude: data.coordinates.lat,
+        longitude: data.coordinates.lon,
         description: data.description,
         displayTitle: data.displaytitle,
         state: state,
         extract: data.extract,
         image: data.originalimage.source,
       }
-      console.log('cityObject',cityObject)
-      setCityDetails(cityObject)
+      setCityDetails({...cityObject})
     })
-    .then(() => getWalkScores(city, state, cityDetails.coordinates.lat, cityDetails.coordinates.lon))
-    .then(data => console.log('walkScores',data))
+    .then(() => getWalkScores(city, state, cityDetails.latitude, cityDetails.longitude))
+    .then(data => {
+      console.log(data)
+      setCityDetails({
+        ...cityDetails,
+        walkScore: data.walkscore,
+        walkDescription: data.description,
+        bikeDescription: data.bike.description,
+        bikeScore: data.bike.score,
+      })
+    })
   }, [city, state])
 
   return ( 
