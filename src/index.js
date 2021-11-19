56d9a4e1692b740978ffe702d7e37f8290c28d32
Carlos Gomez <carlosalbertogomez108@gmail.com>
2021-11-18 19:45:34 -0700

    Install amplify library and imports on index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom'
 import { StyledEngineProvider } from '@mui/material/styles';
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
 
 const router = (
   <BrowserRouter>
